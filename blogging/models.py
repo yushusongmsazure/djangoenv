af103845288310a0354ef1fe4a52940ac8da2fd0
yushusongmsazure <yushusong@outlook.com>
2019-05-14 23:53:03 -0700

    Add tests

--- blogging/models.py
+++ blogging/models.py
@@ -8,3 +8,6 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
