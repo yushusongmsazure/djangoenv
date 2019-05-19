ba3c4d541150dadfe9ce68dadfde6af71b431700
yushusongmsazure <yushusong@outlook.com>
2019-05-19 16:13:15 -0700

    Add login

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,10 +18,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from blogging.views import stub_view, list_view, detail_view
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('polling/', include('polling.urls')),  # <-- Add this
     path('admin/', admin.site.urls),
     path('', include('blogging.urls')),
     path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
 ]
