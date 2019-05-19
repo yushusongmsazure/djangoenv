ef665650cdd0b65f33b1c2b120746d71752d2c9c
yushusongmsazure <yushusong@outlook.com>
2019-05-19 12:49:40 -0700

    Add Category

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # a new import
-from blogging.models import Post
+from blogging.models import Post, Category
 
 # and a new admin registration
 admin.site.register(Post)
+admin.site.register(Category)
