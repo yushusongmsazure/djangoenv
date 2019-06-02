cc81784b5d2a95880e2113d53fb73588cce8e05b
yushusongmsazure <yushusong@outlook.com>
2019-06-02 15:16:54 -0700

    add prod

--- mysite/production.py
+++ mysite/production.py
@@ -9,7 +9,7 @@ DATABASES = {
     'default': dj_database_url.config(default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
     }
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'localhost']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
